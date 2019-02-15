99ca853921f4c5a556c860b5fd095623077543bf
Nick Fujita <nickfujita@gmail.com>
2019-02-15 10:52:20 +0900

    hardcode network

--- src/scripts/utils/dapi.ts
+++ src/scripts/utils/dapi.ts
@@ -4,15 +4,8 @@ import o3dapiNeo from 'o3-dapi-neo';
 o3dapi.initPlugins([o3dapiNeo]);
 
 export function send(amount: string): Promise<any> {
-  return Promise.all([
-    o3dapi.NEO.getNetworks(),
-    o3dapi.NEO.getAccount(),
-  ])
-  .then(([{defaultNetwork}, {address}]) => {
-    // TODO add back in before prod
-    // if (defaultNetwork !== 'MainNet') {
-    //   throw 'O3 app is not set to MainNet.';
-    // }
+  return o3dapi.NEO.getAccount()
+  .then(({address}) => {
     return o3dapi.NEO.send({
       fromAddress: address,
       // TODO add back in before prod
@@ -23,7 +16,7 @@ export function send(amount: string): Promise<any> {
       remark: 'O3CAP',
       // TODO add back in before prod
       // fee: '0.0011',
-      network: defaultNetwork,
+      network: 'MainNet',
     })
     .then(({txid}) => ({address, txid}));
   });
